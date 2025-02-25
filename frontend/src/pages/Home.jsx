d7537a99246546de63c38af3a44d9b51ccd655fd
sana ullah <sunnykhansadda@gmail.com>
2025-02-25 10:23:11 +0500

    done product page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,50 @@ import Hero from "../components/Layout/Hero";
 import GenderCollection from "../components/Products/GenderCollection";
 import NewArravels from "../components/Products/NewArravels";
 import ProductDetails from "../components/Products/ProductDetails";
-import StakingPage from "../components/Products/StakingPage";
+import ProductGrid from "../components/Products/ProductGrid";
+
+const placeholderProducts = [
+  {
+    _id: 1,
+    name: "product 1",
+    price: 100,
+    images: [
+      {
+        url: "https://img.drz.lazcdn.com/static/pk/p/2c72a27b1fd163d96aff8adc8eab7950.jpg_400x400q75.jpg_.webp",
+      },
+    ],
+  },
+  {
+    _id: 2,
+    name: "product 2",
+    price: 100,
+    images: [
+      {
+        url: "https://img.drz.lazcdn.com/static/pk/p/4aaa7a269dfb30fe15140533e9933b8e.jpg_400x400q75.jpg_.webp",
+      },
+    ],
+  },
+  {
+    _id: 3,
+    name: "product 3",
+    price: 100,
+    images: [
+      {
+        url: "https://img.drz.lazcdn.com/static/pk/p/bb8fed17c68c96c55b3de7cddb213b6e.jpg_400x400q75.jpg_.webp",
+      },
+    ],
+  },
+  {
+    _id: 4,
+    name: "product 4",
+    price: 100,
+    images: [
+      {
+        url: "https://img.drz.lazcdn.com/static/pk/p/5bf9d963b7c9c704b230861e866c9af2.jpg_400x400q75.jpg_.webp",
+      },
+    ],
+  },
+];
 
 const Home = () => {
   return (
@@ -14,7 +57,10 @@ const Home = () => {
       {/* Best sELLER */}
       <h2 className="text-3xl text-center font-bold mb-4">Best Seller</h2>
       <ProductDetails />
-      {/* <StakingPage /> */}
+      <div className="container mx-auto">
+        <h2 className="text-3xl text-center font-bold mb-4">Top Wears Women</h2>
+        <ProductGrid product={placeholderProducts} />
+      </div>
     </div>
   );
 };
